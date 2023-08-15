00a27a6501993ad9b124b10b3f1fa1111e67064a
louistsou <louis6251@gmail.com>
2023-08-15 21:00:47 +1000

    map

---
+++ script.js
@@ -0,0 +1,14 @@
+// script.js
+function initMap() {
+    const center = { lat: 37.7749, lng: -122.4194 }; // Set your preferred center coordinates
+    const map = new google.maps.Map(document.getElementById('map'), {
+        center: center,
+        zoom: 12 // Set the initial zoom level
+    });
+
+    const marker = new google.maps.Marker({
+        position: center,
+        map: map,
+        title: 'Marker Title' // Set the marker title
+    });
+}
